61f750ab7428f7ae53b9fb6f575647c2384abd1b
Keleonix <axel@arctique.fr>
2020-05-28 19:46:35 +0200

    Created a Layer 3 calculation IP divided in 10

--- ip/Layer3Calculator.vhd
+++ ip/Layer3Calculator.vhd
@@ -6,8 +6,8 @@ use work.bus_multiplexer_pkg.all;
 
 entity Layer3Calculator is
 	generic(
-		NEURONE_LIST_SIZE_IN	:	natural := 1014;
-		NEURONE_LIST_SIZE_OUT	:	natural := 1014;
+		NEURONE_LIST_SIZE_IN	:	natural := 1255;
+		NEURONE_LIST_SIZE_OUT	:	natural := 1255;
 		NEURONE_SIZE		:	natural := 16
 	);
 	port (
@@ -20,148 +20,138 @@ end Layer3Calculator;
 
 architecture RTL of Layer3Calculator is
 
-signal	i		:	integer;
-signal	j		:	integer;
-signal	k		:	integer;
-signal	m		:	integer;
-signal	n		:	integer;
-signal SAL		:	integer;
-signal sommeInteger	:	integer;
-signal somme		:	std_logic_vector((NEURONE_SIZE*2)-1 downto 0);
-signal mult0		:	std_logic_vector((NEURONE_SIZE*2)-1 downto 0);
-signal mult1		:	std_logic_vector((NEURONE_SIZE*2)-1 downto 0);
-signal mult2		:	std_logic_vector((NEURONE_SIZE*2)-1 downto 0);
-signal mult3		:	std_logic_vector((NEURONE_SIZE*2)-1 downto 0);
-signal mult4		:	std_logic_vector((NEURONE_SIZE*2)-1 downto 0);
-signal mult5		:	std_logic_vector((NEURONE_SIZE*2)-1 downto 0);
 signal nextNeuronesSIG	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
 
-component oneNeuroneCalculator
+component Layer3Divided is
 	generic(
 		NEURONE_LIST_SIZE	:	natural := 1014;
 		NEURONE_SIZE		:	natural := 16
 	);
 	port (
-		i		:	in integer;
-		j		:	in integer;
-		k		:	in integer;
-		m		:	in integer;
-		n		:	in integer;
-		offset		:	in integer;
+		
+		start		:	in integer;
+		finish		:	in integer;
+		calculate	:	in std_logic;
 		neurones	:	in bus_array(0 to NEURONE_LIST_SIZE-1);
 		weights		:	in bus_array(0 to NEURONE_LIST_SIZE-1);
-		mult		:	out std_logic_vector(NEURONE_SIZE-1 downto 0)
+		nextNeurones	:	out bus_array(0 to NEURONE_LIST_SIZE_OUT-1)
 	);
 end component;
 
-component sigmoid
-	port(
-		Z: in integer range 0 to 511;
-		SAL: out integer range 0 to 255
-	);
-end component;
+signal nextNeurones0	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones1	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones2	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones3	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones4	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones5	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones6	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones7	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones8	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
+signal nextNeurones9	:	bus_array(0 to NEURONE_LIST_SIZE_OUT-1);
 
 begin
 
-	neurone0 : oneNeuroneCalculator port map(
-		i 		=> i,
-		j		=> j,
-		k		=> k,
-		m		=> m,
-		n		=> n,
-		offset		=> 0,
-		neurones	=> neurones,
-		weights		=> weights,
-		mult		=> mult0
-	);
-	
-	neurone1 : oneNeuroneCalculator port map(
-		i 		=> i,
-		j		=> j,
-		k		=> k,
-		m		=> m,
-		n		=> n,
-		offset		=> 1,
-		neurones	=> neurones,
-		weights		=> weights,
-		mult		=> mult1
-	);
-
-	neurone2 : oneNeuroneCalculator port map(
-		i 		=> i,
-		j		=> j,
-		k		=> k,
-		m		=> m,
-		n		=> n,
-		offset		=> 2,
-		neurones	=> neurones,
-		weights		=> weights,
-		mult		=> mult2
-	);
-	
-	neurone3 : oneNeuroneCalculator port map(
-		i 		=> i,
-		j		=> j,
-		k		=> k,
-		m		=> m,
-		n		=> n,
-		offset		=> 3,
-		neurones	=> neurones,
-		weights		=> weights,
-		mult		=> mult3
-	);
-
-	neurone4 : oneNeuroneCalculator port map(
-		i 		=> i,
-		j		=> j,
-		k		=> k,
-		m		=> m,
-		n		=> n,
-		offset		=> 4,
-		neurones	=> neurones,
-		weights		=> weights,
-		mult		=> mult4
-	);
-	
-	neurone5 : oneNeuroneCalculator port map(
-		i 		=> i,
-		j		=> j,
-		k		=> k,
-		m		=> m,
-		n		=> n,
-		offset		=> 5,
-		neurones	=> neurones,
-		weights		=> weights,
-		mult		=> mult5
-	);
-
-	sig : sigmoid port map(
-		Z	=> sommeInteger,
-		SAL	=> SAL
+	div0 : Layer3Divided port map(
+		start		=>	0,
+		finish		=>	5,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones0
 	);	
 
+	div1 : Layer3Divided port map(
+		start		=>	5,
+		finish		=>	10,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones1
+	);	
 
-	process
-	begin
-		if(rising_edge(calculate)) then
-			for i in 0 to 50 loop
-				for j in 0 to 5 loop
-					for k in 0 to 5 loop
-						somme <= weights(26*6*i);
-						for m in 0 to 5 loop
-							for n in 0 to 5 loop
-								somme <= somme + mult0 + mult1 + mult2 + mult3 + mult4 + mult5;
-							end loop;
-						end loop;
-						nextNeuronesSIG(5*5*i+5*j+k) <= std_logic_vector(to_unsigned(SAL, 32));
-					end loop;
-				end loop;
-			end loop;
-		end if;
-	end process;
+	div2 : Layer3Divided port map(
+		start		=>	10,
+		finish		=>	15,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones2
+	);	
 
-	sommeInteger <= to_integer(unsigned(somme));
+	div3 : Layer3Divided port map(
+		start		=>	15,
+		finish		=>	20,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones3
+	);	
 
-	nextNeurones <= nextNeuronesSig;
+	div4 : Layer3Divided port map(
+		start		=>	20,
+		finish		=>	25,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones4
+	);	
+
+	div5 : Layer3Divided port map(
+		start		=>	25,
+		finish		=>	30,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones5
+	);	
+
+	div6 : Layer3Divided port map(
+		start		=>	30,
+		finish		=>	35,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones6
+	);	
+
+	div7 : Layer3Divided port map(
+		start		=>	35,
+		finish		=>	40,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones7
+	);	
+
+	div8 : Layer3Divided port map(
+		start		=>	40,
+		finish		=>	45,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones8
+	);	
+
+	div9 : Layer3Divided port map(
+		start		=>	45,
+		finish		=>	50,
+		calculate	=>	calculate,
+		neurones	=>	neurones,
+		weights		=>	weights,
+		nextNeurones	=>	nextNeurones9
+	);	
+
+	
+
+	nextNeurones(0 to 130) <= nextNeurones0(0 to 130);
+	nextNeurones(131 to 255) <= nextNeurones1(131 to 255);
+	nextNeurones(256 to 380) <= nextNeurones2(256 to 380);
+	nextNeurones(381 to 505) <= nextNeurones3(381 to 505);
+	nextNeurones(506 to 630) <= nextNeurones4(506 to 630);
+	nextNeurones(631 to 755) <= nextNeurones5(631 to 755);
+	nextNeurones(756 to 880) <= nextNeurones6(756 to 880);
+	nextNeurones(881 to 1005) <= nextNeurones7(881 to 1005);
+	nextNeurones(1006 to 1130) <= nextNeurones8(1006 to 1130);
+	nextNeurones(1131 to 1255) <= nextNeurones9(1131 to 1255);
 
 end RTL;
